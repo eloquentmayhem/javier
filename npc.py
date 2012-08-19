01cc254328857627295851ee540cae114a593563
Luna <miss.luna.ruan@gmail.com>
2012-08-18 22:42:12 -0400

    npc.py and implementation.txt

---
+++ npc.py
@@ -0,0 +1,26 @@
+import container
+
+class Npc(container.Container):
+    def __init__(self, name, description, takeable, nTakeDesc):
+        super(Npc, self).__init__(name, description)
+        #true if you can take obj at the moment, else false
+        self.takeable = takeable
+        #description if you try to take an obj that can't be
+        #take at the moment
+        self.nTakeDesc = nTakeDesc
+
+    def is_takeable(self):
+        return self.takeable
+        
+    def set_takeable(self, takeable):
+        self.takeable = takeable        
+        
+    def print_nTakeDesc(self):
+        print(self.nTakeDesc)
+        
+    def give_item(self, item):
+        player.player.add_elem(item)
+        self.delete_elem(item)
+        
+    def use_item(self, item):
+        self.delete_item(item)
